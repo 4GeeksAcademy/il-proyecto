b2a1c8eef73815348a05d4b7e453bbd02ca1608c
Natalia <nlopezsalas@gmail.com>
2024-04-07 20:30:28 +0000

    Add google sign up button

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,14 +1,56 @@
-import React, { useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
+
 export const Home = () => {
 	const { store, actions } = useContext(Context);
+
+	function handleCallbackResponse(response) {
+		console.log("Encoded JWT ID token: " + response.credential);
+	}
+
+	useEffect(() => {
+
+		const isApiScriptLoaded = document.querySelector('script[src="https://accounts.google.com/gsi/client"]');
+
+		if (!isApiScriptLoaded) {
+			// Crea un nuevo elemento script
+			const script = document.createElement('script');
+			script.src = "https://accounts.google.com/gsi/client";
+			script.async = true;
+			script.defer = true;
+			script.onload = () => {
+				// El script se ha cargado y está listo para usar
+				console.log("Google API script loaded successfully.");
+				// Aquí puedes inicializar o ejecutar tu lógica relacionada con la API de Google
+			};
+			script.onerror = () => {
+				console.error("Error loading the Google API script.");
+			};
+			// Añade el script al documento
+			document.body.appendChild(script);
+		}
+
+
+		/* global google */
+		google.accounts.id.initialize({
+			client_id: "364011413939-4bmaa89l50nceb4c0cgapc2vsn5qh0p2.apps.googleusercontent.com",
+			callback: handleCallbackResponse
+
+		})
+		google.accounts.id.renderButton(
+			document.getElementById("signInDiv"),
+			{ theme: "outline", size: "large" }
+		)
+	}, []);
 
 	return (
 		<div className="text-center mt-5">
 			<h1>Welcome to Mymood</h1>
+
+			<div id="signInDiv"></div>
 			<p>
 				<img src={rigoImageUrl} />
 			</p>
