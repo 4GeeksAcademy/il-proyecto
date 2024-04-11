429b470c56d34b7587b305b2aaf2ab8398fe136f
Natalia <nlopezsalas@gmail.com>
2024-04-11 18:01:44 +0000

    Add new database

--- src/api/models.py
+++ src/api/models.py
@@ -3,10 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(255))
+    surnames = db.Column(db.String(255))
+    age = db.Column(db.Integer)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    is_active = db.Column(db.Boolean)
+    profile_url = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime)
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+    hobbie_id = db.Column(db.Integer, db.ForeignKey('hobbie.id'))
+    mood_id = db.Column(db.Integer, db.ForeignKey('mood.id'))
+    phycologyst_id = db.Column(db.Integer, db.ForeignKey('phycologyst.id'))
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +25,211 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "surnames": self.surnames,
+            "age": self.age,
+            "is_active": self.is_active,
+            "profile_url": self.profile_url,
+            # Do not serialize the password, it's a security breach
         }
+
+class Location(db.Model):
+    __tablename__ = 'location'
+    id = db.Column(db.Integer, primary_key=True)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    users = db.relationship('User', backref='location', lazy=True)
+    
+    def __repr__(self):
+        return f'<Location {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+        }
+        
+class Hobbie(db.Model):
+    __tablename__ = 'hobbie'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User', backref='hobbie', lazy=True)
+
+    def __repr__(self):
+        return f'<Hobbie {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+class CategoryMood(db.Model):
+    __tablename__ = 'category_mood'
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(255))
+    moods = db.relationship('Mood', backref='category_mood', lazy=True, uselist=False)
+    
+    def __repr__(self):
+        return f'<CategoryMood {self.category}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "category": self.category,
+        }
+
+class Mood(db.Model):
+    __tablename__ = 'mood'
+    id = db.Column(db.Integer, primary_key=True)
+    mood = db.Column(db.String(255))
+    category_id = db.Column(db.Integer, db.ForeignKey('category_mood.id'), unique=True)
+    description = db.Column(db.String(255))
+    users = db.relationship('User', backref='mood', lazy=True)
+    
+    def __repr__(self):
+        return f'<Mood {self.mood}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "mood": self.mood,
+            "description": self.description,
+        }
+
+class UserMoodHistory(db.Model):
+    __tablename__ = 'user_mood_history'
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    date = db.Column(db.Date, primary_key=True)
+    mood_id = db.Column(db.Integer, db.ForeignKey('mood.id'))
+    user = db.relationship('User', backref='user_mood_history')
+    mood = db.relationship('Mood')
+    
+    def __repr__(self):
+        return f'<UserMoodHistory {self.user_id} {self.date}>'
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "date": self.date,
+            "mood_id": self.mood_id,
+        }
+
+class Action(db.Model):
+    __tablename__ = 'actions'
+    id = db.Column(db.Integer, primary_key=True)
+    action = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    category_id = db.Column(db.Integer, db.ForeignKey('category_mood.id'))
+
+    category = db.relationship('CategoryMood', backref='actions')
+
+    def __repr__(self):
+        return f'<Action {self.action}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "action": self.action,
+            "description": self.description,
+            "category_id": self.category_id,
+        }
+
+class ResourceType(db.Model):
+    __tablename__ = 'resource_type'
+    id = db.Column(db.Integer, primary_key=True)
+    resource_type = db.Column(db.String(255))
+    
+    def __repr__(self):
+        return f'<ResourceType {self.resource_type}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "resource_type": self.resource_type,
+        }
+
+class Resource(db.Model):
+    __tablename__ = 'resource'
+    id = db.Column(db.Integer, primary_key=True)
+    resource_type_id = db.Column(db.Integer, db.ForeignKey('resource_type.id'))
+    url = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    phycologyst_id = db.Column(db.Integer, db.ForeignKey('phycologyst.id'))
+    resource_type = db.relationship('ResourceType', backref='resource')
+    phycologyst = db.relationship('Phycologyst', backref='resource')
+    
+    def __repr__(self):
+        return f'<Resource {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "resource_type_id": self.resource_type_id,
+            "url": self.url,
+            "description": self.description,
+        }
+
+class Chat(db.Model):
+    __tablename__ = 'chat'
+    id = db.Column(db.Integer, primary_key=True)
+    user_sender_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_reciver_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    message_text = db.Column(db.String(255))
+    time = db.Column(db.DateTime)
+    sender = db.relationship('User', foreign_keys=[user_sender_id])
+    receiver = db.relationship('User', foreign_keys=[user_reciver_id])
+    
+    def __repr__(self):
+        return f'<Chat {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_sender_id": self.user_sender_id,
+            "user_reciver_id": self.user_reciver_id,
+            "message_text": self.message_text,
+            "time": self.time,
+        }
+
+class Phycologyst(db.Model):
+    __tablename__ = 'phycologyst'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    surnames = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    experience = db.Column(db.Integer)
+    users = db.relationship('User', backref='phycologyst', lazy=True)
+    
+    def __repr__(self):
+        return f'<Phycologyst {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "surnames": self.surnames,
+            "email": self.email,
+            "experience": self.experience,
+        }
+
+class Sessions(db.Model):
+    __tablename__ = 'sessions'
+    id = db.Column(db.Integer, primary_key=True)
+    phycologyst_id = db.Column(db.Integer, db.ForeignKey('phycologyst.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    phycologyst = db.relationship('Phycologyst', backref='sessions')
+    user = db.relationship('User', backref='sessions')
+    
+    def __repr__(self):
+        return f'<Sessions {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "phycologyst_id": self.phycologyst_id,
+            "user_id": self.user_id,
+        }
+
