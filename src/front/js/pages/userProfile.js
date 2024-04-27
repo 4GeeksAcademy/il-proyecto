e8f6f974df0c918781c906242ed8628e912f66f1
Natalia <nlopezsalas@gmail.com>
2024-04-27 08:13:15 +0000

    update profile users

--- src/front/js/pages/userProfile.js
+++ src/front/js/pages/userProfile.js
@@ -12,21 +12,56 @@ import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Form from 'react-bootstrap/Form';
+
 
 
 export const UserProfile = () => {
     const { store, actions } = useContext(Context);
+    const [show, setShow] = useState(false);
+    const [message, setMessage] = useState('');
+    const [isError, setIsError] = useState(true);
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [createdAt, setCreatedAt] = useState("");
+
 
     useEffect(() => {
-        
+
     }, []);
 
     console.log(store.user);
-    
-    if (!store.user || Object.keys(store.user).length === 0) {
-        return null; // O puedes retornar un componente de carga
-      }
 
+    // if (!store.user || Object.keys(store.user).length === 0) {
+    //     return null; // O puedes retornar un componente de carga
+    // }
+    if (!store.user || !store.user.psychologists || store.user.psychologists.length === 0) {
+        return null; // O puedes retornar un componente de carga
+    }
+
+    let created_at = store.user.created_at;
+    let dateOnly = created_at.split(' ')[0];
+
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+
+    const halfwayIndex = Math.ceil(store.user.psychologists.length / 2);
+    const firstHalf = store.user.psychologists.slice(0, halfwayIndex);
+    const secondHalf = store.user.psychologists.slice(halfwayIndex);
+
+    // const handleResetPassword = async (e) => {
+    // 	e.preventDefault();
+    // 	const response = await actions.handleResetPassword(email);
+    // 		if (!response.ok) {
+    // 			setIsError(true);
+    // 			setMessage(result.message || "Ocurrió un error. Por favor, inténtalo de nuevo.");
+    // 		} else {
+    // 			setIsError(false);
+    // 			setMessage("Consulta tu email para las instrucciones de reestablecimiento de contraseña.");
+    // 			console.log('Reset link sent successfully!');
+    // 		}
+    // };
 
     return (
         <Container fluid className="container-landingpage">
@@ -46,8 +81,11 @@ export const UserProfile = () => {
                         <Card className="user-info-profile">
                             <ListGroup variant="flush">
                                 <ListGroup.Item><p>Email</p><p>{store.user.email}</p></ListGroup.Item>
-                                <ListGroup.Item><p>Contraseña</p><p>Cambiar contraseña</p></ListGroup.Item>
-                                <ListGroup.Item><p>Unido/a</p><p>24/04/2024</p></ListGroup.Item>
+                                <ListGroup.Item><p>Contraseña</p>
+                                    {/* <a variant="primary" onClick={handleShow} className="text-black text-end link"> Cambiar mi contraseña</a> */}
+                                    <a href="/login">Cambiar mi contraseña</a>
+                                </ListGroup.Item>
+                                <ListGroup.Item><p>Unido/a</p><p>{dateOnly}</p></ListGroup.Item>
                             </ListGroup>
                         </Card>
                     </Col>
@@ -72,9 +110,9 @@ export const UserProfile = () => {
                 <Row>
                     <Col className="resources">
                         <ul>
-                            <li class="articulo">Artículo - Título del recurso (Nombre psicólogo)</li>
-                            <li class="video">Vídeo - Título del recurs (Nombre psicólogo)</li>
-                            <li class="podcast">Podcast - Título del recurso (Nombre psicólogo)</li>
+                            <li className="articulo">Artículo - Título del recurso (Nombre psicólogo)</li>
+                            <li className="video">Vídeo - Título del recurs (Nombre psicólogo)</li>
+                            <li className="podcast">Podcast - Título del recurso (Nombre psicólogo)</li>
                         </ul>
                     </Col>
                 </Row>
@@ -83,27 +121,83 @@ export const UserProfile = () => {
                         <h4 className="border-bottom border-dark mb-3">Mis psicólogos de referencia</h4>
                     </Col>
                 </Row>
-                <Row>
+                {/* <Row>
                     <Col xs={12} md={6} lg={3} className="text-center">
-                        <Image src="https://firebasestorage.googleapis.com/v0/b/my-mood-507ca.appspot.com/o/Psychologists%20profiles%2Fpsychologists-1b.webp?alt=media&token=05bc6a22-e3f8-42d4-9ab5-426a2fe72cb4" className="img-mood" roundedCircle/>
+                        <Image src="https://firebasestorage.googleapis.com/v0/b/my-mood-507ca.appspot.com/o/Psychologists%20profiles%2Fpsychologists-1b.webp?alt=media&token=05bc6a22-e3f8-42d4-9ab5-426a2fe72cb4" className="img-mood" roundedCircle />
                         <p>nombre psicólogo</p>
                     </Col>
                     <Col xs={12} md={6} lg={3} className="text-center">
-                        <Image src="https://firebasestorage.googleapis.com/v0/b/my-mood-507ca.appspot.com/o/Psychologists%20profiles%2Fpsychologists-4g.webp?alt=media&token=1dfe2334-1f82-4cb2-a8d2-2206b027fc31" className="img-mood" roundedCircle/>
-                        <p>nombre psicólogo</p>
-                    </Col>
-                    <Col xs={12} md={6} lg={3} className="text-center">
-                        <Image src="https://firebasestorage.googleapis.com/v0/b/my-mood-507ca.appspot.com/o/Psychologists%20profiles%2Fpsychologists-profile.webp?alt=media&token=2363922b-b6ca-451d-be3f-197185a61beb" className="img-mood"roundedCircle/>
+                        <Image src="https://firebasestorage.googleapis.com/v0/b/my-mood-507ca.appspot.com/o/Psychologists%20profiles%2Fpsychologists-4g.webp?alt=media&token=1dfe2334-1f82-4cb2-a8d2-2206b027fc31" className="img-mood" roundedCircle />
                         <p>nombre psicólogo</p>
                     </Col>
                     <Col xs={12} md={6} lg={3} className="text-center">
                         <Image src="https://firebasestorage.googleapis.com/v0/b/my-mood-507ca.appspot.com/o/Psychologists%20profiles%2Fpsychologists-profile.webp?alt=media&token=2363922b-b6ca-451d-be3f-197185a61beb" className="img-mood" roundedCircle />
                         <p>nombre psicólogo</p>
                     </Col>
+                    <Col xs={12} md={6} lg={3} className="text-center">
+                        <Image src="https://firebasestorage.googleapis.com/v0/b/my-mood-507ca.appspot.com/o/Psychologists%20profiles%2Fpsychologists-profile.webp?alt=media&token=2363922b-b6ca-451d-be3f-197185a61beb" className="img-mood" roundedCircle />
+                        <p>nombre psicólogo</p>
+                    </Col>
+                </Row> */}
+                <Row>
+                    <Col xs={12} md={6} lg={6} className="text-center">
+                        {firstHalf.map(psychologist => (
+                            <div className="ps-card p-3" key={psychologist.id}>
+                                <Image src={psychologist.profile_url} className="img-mood" roundedCircle />
+                                <p className="fw-bold">{psychologist.name} {psychologist.surnames}</p>
+                                <p className="small">{psychologist.biography}</p>
+                                <a href="#" className="small btn btn-dark btn-sm rounded-pill">Ver perfil &rarr;</a>
+                            </div>
+                        ))}
+                    </Col>
+                    <Col xs={12} md={6} lg={6} className="text-center">
+                        {secondHalf.map(psychologist => (
+                            <div key={psychologist.id}>
+                                <Image
+                                    src={psychologist.profile_url || "https://firebasestorage.googleapis.com/v0/b/my-mood-507ca.appspot.com/o/Psychologists%20profiles%2Fpsychologists-profile.webp?alt=media&token=2363922b-b6ca-451d-be3f-197185a61beb"}
+                                    className="img-mood"
+                                    roundedCircle
+                                />
+                                <p>{psychologist.name} {psychologist.surnames}</p>
+                                <p>{psychologist.biography}</p>
+                            </div>
+                        ))}
+                    </Col>
                 </Row>
             </Container>
+            {/* <Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title className="heading1">Quiero cambiar mi contraseña</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form onSubmit={handleResetPassword}>
+						<Row>
+							<Col sm={12}>
+								<Form.Control
+									type="email"
+									placeholder="Enter email"
+									value={email}
+									onChange={e => setEmail(e.target.value)}
+									required
+								/>
+							</Col>
+							<Col>
+								<button type="submit" className="button1 form-button mt-3" >Envíame el enlace</button>
+							</Col>
+						</Row>
+					</Form>
+					<Row>
+						<Col sm={12}>
+							{message && (
+								<div className={`mt-3 border p-3 ${isError ? "border-danger text-danger" : "border-success text-success"}`}>
+									{message}
+								</div>
+							)}
+						</Col>
+					</Row>
 
-
-        </Container >
+				</Modal.Body>
+			</Modal> */}
+        </Container>
     );
 };
