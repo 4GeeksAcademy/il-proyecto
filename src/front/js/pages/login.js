49f696adb3f574daf6bc7c0b1a85a2b0b052e52c
Natalia <nlopezsalas@gmail.com>
2024-05-02 22:20:08 +0000

    delete comments

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -22,9 +22,6 @@ import Modal from 'react-bootstrap/Modal';
 
 
 const MyTextInput = ({ label, ...props }) => {
-	// useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-	// which we can spread on <input>. We can use field meta to show an error
-	// message if the field is invalid and it has been touched (i.e. visited)
 	const [field, meta] = useField(props);
  	
 	return (
@@ -38,7 +35,6 @@ const MyTextInput = ({ label, ...props }) => {
 	);
 };
 
-// And now we can use these
 export const Login = () => {
 	const [error, setError] = useState("");
 	const { actions } = useContext(Context);
@@ -80,7 +76,6 @@ export const Login = () => {
 				setIsError(false);
 				setMessage("Consulta tu email para las instrucciones de reestablecimiento de contraseña.");
 				console.log('Reset link sent successfully!');
-				// navigate("/login");  // Optionally redirect to login
 			}
 	};
 
@@ -218,32 +213,6 @@ export const Login = () => {
 						</Col>
 					</Row> 
 
-					{/* <Form onSubmit={handleResetPassword}>
-						<Row>
-							<Col sm={12}>
-								<Form.Control
-									type="email"
-									placeholder="Enter email"
-									value={email}
-									onChange={e => setEmail(e.target.value)}
-									required
-								/>
-							</Col>
-							<Col>
-								<button type="submit" className="button1 form-button mt-3" >Envíame el enlace</button>
-							</Col>
-						</Row>
-					</Form>
-					<Row>
-						<Col sm={12}>
-							{message && (
-								<div className={`mt-3 border p-3 ${isError ? "border-danger text-danger" : "border-success text-success"}`}>
-									{message}
-								</div>
-							)}
-						</Col>
-					</Row> */}
-
 				</Modal.Body>
 			</Modal>
 			
@@ -253,137 +222,4 @@ export const Login = () => {
 
 export default Login;
 
-
-
-// export const Login = () => {
-	// const { actions } = useContext(Context);
-	// const [email, setEmail] = useState("");
-	// const [password, setPassword] = useState("");
-	// const [error, setError] = useState("");
-	// const [message, setMessage] = useState('');
-	// const [isError, setIsError] = useState(true);
-	// const navigate = useNavigate();
-
-	// const [show, setShow] = useState(false);
-
-	// const handleClose = () => setShow(false);
-	// const handleShow = () => setShow(true);
-
-// 	const handleSubmit = async (e) => {
-// 		e.preventDefault();
-// 		setError("");
-// 		let logged = await actions.login(email, password);
-// 		if (logged) {
-// 			navigate("/choose-mood");
-// 		} else {
-// 			setError("Failed to log in. Please check your email and password.");
-// 		}
-// 	};
-
-// 	const handleGoogleSuccess = async (response) => {
-// 		setError("");
-// 		console.log(response);
-// 		let loggedWithGoogle = await actions.loginGoogle(response);
-// 		if (loggedWithGoogle) {
-// 			navigate('/choose-mood');
-// 		}
-// 		else {
-// 			setError("Failed to log in with Google. Please check your email and password.");
-// 		}
-// 	};
-
-// 	const handleFailure = (error) => {
-// 		console.log(error);
-// 		setError("Failed to log in. Please check your email and password.");
-// 	};
-
-// 	const handleResetPassword = async (e) => {
-// 		e.preventDefault();
-// 		const response = await actions.handleResetPassword(email)	
-// 			if (!response.ok) {
-// 				setIsError(true);
-// 				setMessage(result.message || "Ocurrió un error. Por favor, inténtalo de nuevo.");
-// 			} else {
-// 				setIsError(false);
-// 				setMessage("Consulta tu email para las instrucciones de reestablecimiento de contraseña.");
-// 				console.log('Reset link sent successfully!');
-// 				// navigate("/login");  // Optionally redirect to login
-// 			}
-// 	};
-	
-// 	return (
-// 		<>
-
-// 			<Container fluid className="container-landingpage">
-// 				<Row className="mt-3">
-// 					<Col>
-// 						<h1 className="heading1">Iniciar sesión</h1>
-// 						<Form onSubmit={handleSubmit}>
-// 							<Form.Group className="mb-3" controlId="formBasicEmail">
-// 								<Form.Label>Email</Form.Label>
-// 								<Form.Control type="email" placeholder="Enter email" onChange={e => setEmail(e.target.value)} />
-// 							</Form.Group>
-// 							<Form.Group className="mb-3" controlId="formBasicPassword">
-// 								<Form.Label>Contraseña</Form.Label>
-// 								<Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
-// 							</Form.Group>
-// 							{/* <a href="/reset-password" className="link">He olvidado mi contraseña</a> */}
-// 							<a variant="primary" onClick={handleShow} className="text-black text-end link">
-// 								He olvidado mi contraseña
-// 							</a>
-// 							<button type="submit" className="button1 form-button mb-3">¡Entrar a Mymood!</button>
-// 							<p className="text-center" style={{ fontFamily: 'var(--base-font-family)' }}>¿No tienes cuenta? <a variant="primary" href="/singup" className="text-black">Regístrate </a></p>
-// 						</Form>
-// 						{error && <div className="text-danger mt-3 border border-danger p-3">{error}</div>}
-// 					</Col>
-// 				</Row>
-
-			// 	<Row className="text-center mt-3 mb-5">
-			// 		<Col>
-			// 			<p>O entrar con:</p>
-			// 			<GoogleProvider>
-			// 				<div >
-			// 					<GoogleLogin onSuccess={handleGoogleSuccess} onError={handleFailure} className="button1 form-button" />
-			// 				</div>
-			// 			</GoogleProvider>
-			// 		</Col>
-			// 	</Row>
-			// </Container >
-
-			// <Modal show={show} onHide={handleClose}>
-			// 	<Modal.Header closeButton>
-			// 		<Modal.Title className="heading1">Quiero recuperar mi contraseña</Modal.Title>
-			// 	</Modal.Header>
-			// 	<Modal.Body>
-			// 		<Form onSubmit={handleResetPassword}>
-			// 			<Row>
-			// 				<Col sm={12}>
-			// 					<Form.Control
-			// 						type="email"
-			// 						placeholder="Enter email"
-			// 						value={email}
-			// 						onChange={e => setEmail(e.target.value)}
-			// 						required
-			// 					/>
-			// 				</Col>
-			// 				<Col>
-			// 					<button type="submit" className="button1 form-button mt-3" >Envíame el enlace</button>
-			// 				</Col>
-			// 			</Row>
-			// 		</Form>
-			// 		<Row>
-			// 			<Col sm={12}>
-			// 				{message && (
-			// 					<div className={`mt-3 border p-3 ${isError ? "border-danger text-danger" : "border-success text-success"}`}>
-			// 						{message}
-			// 					</div>
-			// 				)}
-			// 			</Col>
-			// 		</Row>
-
-			// 	</Modal.Body>
-			// </Modal>
-// 		</>
-// 	);
-// };
 
